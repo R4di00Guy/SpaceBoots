f1499669159b76ee3852dbcea175461bf41e8191
R4di00Guy <ruslan_pesenko@zsgorzyce.edu.pl>
2025-11-06 21:53:51 +0100

    Made theme switch and added it to every page

--- scripts.js
+++ scripts.js
@@ -1,1 +1,24 @@
+//day/night switch themeSwitch
 
+var l=0;
+$(function(){
+    $('#themeSwitch').click(function(){
+        if(l==0){
+        $('body').css({//changes light to dark
+            'background-image': 'url("SpaceBoots_logo.png")',
+            'color':'white',
+        });
+        l=1;
+        console.log(l);
+    }
+        else{
+            $('body').css({//changes dark to light
+            'background-image': 'url("bckgrnd_light.jpg")',
+            'color':'black',
+        });
+        l=0;
+        console.log(l);
+    }
+
+    })
+})
