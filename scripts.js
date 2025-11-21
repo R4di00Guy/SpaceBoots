b96ba7aa5d1aa608b237d734507f30956871fb7c
makrony <uvarvara81@gmail.com>
2025-11-21 20:08:53 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -5,7 +5,7 @@ $(function(){
     $('#themeSwitch').click(function(){
         if(l==0){
         $('body').css({//changes light to dark
-            'background-image': 'url("SpaceBoots_logo.png")',
+            'background-image': 'url("eds_bcc.gif")',
             'color':'white',
         });
         $('#themeSwitch').text("⏾");
