95b365c30a9ac858ac438a688ff0258f9ad73eb1
makrony <uvarvara81@gmail.com>
2026-01-03 18:49:03 +0100

    filters on homepage

--- index.php
+++ index.php
@@ -1,10 +1,39 @@
-
 <?php
-// session_start();
+session_start(); 
 include 'database/Connection.php';
 
-$query = "SELECT id_product, p_name, p_price, p_color, p_collection FROM products";
-$result = $db->query($query);
+$collections = $db->query("SELECT DISTINCT p_collection FROM products");
+$colors = $db->query("SELECT DISTINCT colors FROM sizes_colors");
+$sizes = $db->query("SELECT DISTINCT sizes FROM sizes_colors ORDER BY sizes ASC");
+
+$sql = "SELECT DISTINCT p.id_product, p.p_name, p.p_collection, MIN(p.p_price) as p_price 
+        FROM products p 
+        LEFT JOIN sizes_colors sc ON p.id_product = sc.id_product 
+        WHERE 1=1";
+
+if (!empty($_GET['type'])) {
+    $type = $db->real_escape_string($_GET['type']);
+    $sql .= " AND p.p_collection = '$type'";
+}
+
+if (!empty($_GET['colour'])) {
+    $colour = $db->real_escape_string($_GET['colour']);
+    $sql .= " AND sc.colors = '$colour'";
+}
+
+if (!empty($_GET['size'])) {
+    $size = (int)$_GET['size'];
+    $sql .= " AND sc.sizes = $size";
+}
+
+if (!empty($_GET['price_en'])) {
+    $price_to = (float)$_GET['price_en'];
+    $sql .= " AND p.p_price <= $price_to";
+}
+
+$sql .= " GROUP BY p.id_product";
+
+$result = $db->query($sql);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -30,8 +59,8 @@ $result = $db->query($query);
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Gelasio:ital,wght@0,400..700;1,400..700&display=swap" rel="stylesheet">
-<script src="JavaScript/jquery-3.7.1.js"></script>
-<script src="JavaScript/scripts.js"></script>
+    <script src="JavaScript/jquery-3.7.1.js"></script>
+    <script src="JavaScript/scripts.js"></script>
     <style>
         .container {
             margin: 20px 0px 8px 16px;
@@ -50,7 +79,7 @@ $result = $db->query($query);
         .product_img_cont{
             width: 142%;
             position: relative;
-            max-height: 300px;
+            max-height: 150px;
             margin-bottom: 14px;
         }
         .product_img{
@@ -78,13 +107,13 @@ $result = $db->query($query);
             while($row = $result->fetch_assoc()) {
                 ?>
                 <div class="container">
-                    <h3><?php echo htmlspecialchars($row['p_name']); ?></h3>
+                    <h5 style="font-size: 28px; margin: 0px; padding: 0px; margin-top: 6px"><?php echo htmlspecialchars($row['p_name']); ?></h5>
                     
                     <div class="product_img_cont">
                         <img class="product_img" src="images/buty/but<?php echo $row['id_product']; ?>.png" alt="boots">
                     </div>
                     
-                    <h4><?php echo $row['p_price']; ?> zl</h4>
+                    <h4><?php echo $row['p_price']; ?> zł</h4>
                     
                     <?php if(isset($_SESSION['username'])): ?>
                          <button style="font-family: 'Inter'; font-size: 10px; cursor: pointer;">Add to cart</button>
@@ -105,36 +134,58 @@ $result = $db->query($query);
     </style>
     <div class="filters">
         <h1>Filters:</h1>
-        <table>
-            <tr>
-                <td><label for="size">Size:</label></td>
-                <td><input type="number" name="size" min="30" max="49"></td>
-            </tr>
-            <tr>
-                <td><label for="colour">Colour:</label></td>
-                <td><select name="colour">
-                    <option value="">Any</option>
-                    <option value="Black">Black</option>
-                    <option value="White">White</option>
-                    <option value="Blue">Blue</option>
-                    <option value="Brown">Brown</option>
-                </select></td>
-            </tr>
-            <tr>
-                <td><label for="Type">Type:</label></td>
-                <td><select name="type">
-                    <option value="">Any</option>
-                    <option value="Sport">Sport</option>
-                    <option value="Buisness">Buisness</option>
-                    <option value="Winter">Winter</option>
-                    <option value="Casual">Casual</option>
-                </select></td>
-            </tr>
-            <tr>
-                <td><label for="price">Price:</label></td>
-                <td><input type="number" name="price_st"><span style='font-family: "Gelasio"; margin: 0 5px;'>to</span><input type="number" name="price_en"></td>
-            </tr>
-        </table>
+        <form method="GET" action="index.php">
+            <table>
+                <tr>
+                    <td><label for="size">Size:</label></td>
+                    <td>
+                        <select name="size">
+                            <option value="">Any</option>
+                            <?php while($s = $sizes->fetch_assoc()): ?>
+                                <option value="<?php echo $s['sizes']; ?>" <?php if(isset($_GET['size']) && $_GET['size'] == $s['sizes']) echo 'selected'; ?>>
+                                    <?php echo $s['sizes']; ?>
+                                </option>
+                            <?php endwhile; ?>
+                        </select>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td><label for="colour">Colour:</label></td>
+                    <td>
+                        <select name="colour">
+                            <option value="">Any</option>
+                            <?php while($c = $colors->fetch_assoc()): ?>
+                                <option value="<?php echo $c['colors']; ?>" <?php if(isset($_GET['colour']) && $_GET['colour'] == $c['colors']) echo 'selected'; ?>>
+                                    <?php echo $c['colors']; ?>
+                                </option>
+                            <?php endwhile; ?>
+                        </select>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td><label for="type">Type:</label></td>
+                    <td>
+                        <select name="type">
+                            <option value="">Any</option>
+                            <?php while($coll = $collections->fetch_assoc()): ?>
+                                <option value="<?php echo $coll['p_collection']; ?>" <?php if(isset($_GET['type']) && $_GET['type'] == $coll['p_collection']) echo 'selected'; ?>>
+                                    <?php echo $coll['p_collection']; ?>
+                                </option>
+                            <?php endwhile; ?>
+                        </select>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td><label>Price to:</label></td>
+                    <td><input type="number" name="price_en" value="<?php echo $_GET['price_en'] ?? ''; ?>"></td>
+                </tr>
+            </table>
+                <button type="submit" style="margin-top: 10px; cursor: pointer;">Apply Filters</button>
+                <a href="index.php" style="font-size: 12px; margin-left: 10px;">Reset</a>
+        </form>
     </div>
     <footer id="homef">
         <div class="footerlogo">
