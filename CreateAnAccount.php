4cbedec5a8180d180c2279eacdc15c9a447cc197
bartek979 <bartosz_koper@zsgorzyce.edu.pl>
2026-01-05 22:49:53 +0100

    Update CreateAnAccount.php

--- CreateAnAccount.php
+++ CreateAnAccount.php
@@ -104,7 +104,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 </head>
 <body>
     <header>
-        <a href="index.php" class="logo"><img src="images/SpaceBoots_logo2.png"></a>
+        <a href="index.php" class="logo"><img src="images/SpaceBoots_logo2.png" alt="SpaceBoots Logo"></a>
         <!--logo--> 
         <nav>
                 <a href="ShoppingCart.php" class="nav"><img id="cart" alt="shopping cart" src='images/ikony/koszyk.png'></a><div class="spaces"></div>
@@ -166,4 +166,5 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 </body>
 
 </html>
+
 
